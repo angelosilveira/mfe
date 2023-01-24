25d3f26d20c72c08b66521895f6783caf45ebc8e
angelo <angelo.silveira09@gmail.com>
2023-01-24 01:46:21 -0300

    implement mfe auth

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,7 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -16,6 +15,7 @@ export default () => {
           history.push(nextPathname);
         }
       },
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
